fa8ac6e2b9d3a13fe25c2f4bbc9e10e93b11642f
Aayush Patel <74599395+Aayush2492@users.noreply.github.com>
2021-10-25 17:30:38 +0530

    Used setState Hook to implement counter using functional component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./App.css";
 
-import "./components/ClassCounter";
 import ClassCounter from "./components/ClassCounter";
+import HookCounter from "./components/HookCounter";
 
 const App = () => {
   return (
     <div className="App">
       <h1>HELLo world </h1>
       <ClassCounter />
+      <HookCounter />
     </div>
   );
 };
