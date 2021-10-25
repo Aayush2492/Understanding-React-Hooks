643944e812dfe6b365c58939ac0797802230b836
Aayush Patel <74599395+Aayush2492@users.noreply.github.com>
2021-10-25 17:17:23 +0530

    Implemented counter with stateful class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import "./components/ClassCounter";
+import ClassCounter from "./components/ClassCounter";
+
 const App = () => {
   return (
     <div className="App">
       <h1>HELLo world </h1>
+      <ClassCounter />
     </div>
   );
 };
