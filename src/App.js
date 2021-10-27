b761646353080e636860d417bb79ce3bd16ffaca
Aayush Patel <74599395+Aayush2492@users.noreply.github.com>
2021-10-27 19:15:34 +0530

    useState on array

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import ClassCounter from "./components/ClassCounter";
 import HookCounter from "./components/HookCounter";
 import HookWithObject from "./components/HookWithObject";
+import HookWithArray from "./components/HookWithArray";
 
 const App = () => {
   return (
@@ -11,7 +12,8 @@ const App = () => {
       <h1>HELLo world </h1>
       {/* <ClassCounter /> */}
       {/* <HookCounter /> */}
-      <HookWithObject />
+      {/* <HookWithObject /> */}
+      <HookWithArray />
     </div>
   );
 };
